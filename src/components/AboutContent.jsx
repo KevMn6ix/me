e6e9b281f4a8f7f6715b0639b49142afe684253c
KevMn6ix <kevin.munto@efrei.net>
2024-01-12 18:07:44 +0100

    fixing the pdf issue

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -3,22 +3,9 @@ import React from 'react'
 import "../styles/AboutContent.css"
 import { Link } from 'react-router-dom'
 import Kevin from "../assets/kevinm.jpg"
-import { FaPython,FaJs,FaJava, FaReact, FaGitAlt, FaDatabase  } from 'react-icons/fa'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CV from "../assets/MuntoKevinCV.pdf";
 
-function AboutContent()  {
-    const SIZE = 50;
-    /*
-    const cv = () => {
-        const cvUrl = "../assets/MuntoKevinCV.pdf";
-        const link = document.createElement("a");
-        link.href = cvUrl;
-        link.download = "MuntoKevinCV.pdf";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);  
-    };*/
-
+function AboutContent()  {       
     return (
         <div className="about">
             <div className="left">
@@ -39,15 +26,9 @@ function AboutContent()  {
                 <Link to="/contact/">
                     <button className='btn'>Contact</button>
                 </Link>
-
-                {<Link to="../assets/MuntoKevinCV.pdf" target='_blank' download= "MuntoKevinCV.pdf">
-                    <button className='btn' >Download My Cv</button>
-                </Link> }
-                
-                {/*<a href="../assets/MuntoKevinCV.pdf" download="MuntoKevinCV.pdf">
-                    <button className='btn' >Download My Cv</button>
-                </a>*/}
-                
+                <a href={CV} download="MuntoKevinCV.pdf">
+                    <button className='btn'>Download My Cv</button>
+                </a>                
             </div>
             
             {/*<div className="right">
@@ -62,5 +43,3 @@ function AboutContent()  {
 };
 
 export default AboutContent
-
-// <button className='btn' onClick={cv} href="../assets/MuntoKevinCV.pdf">Download My Cv</button>
