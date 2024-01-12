b080387f83811ed3f8275c92ac7ea54828464325
KevMn6ix <kevin.munto@efrei.net>
2024-01-12 15:43:34 +0100

    editing resume

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -5,33 +5,51 @@ import { Link } from 'react-router-dom'
 import Kevin from "../assets/kevinm.jpg"
 
 function AboutContent()  {
-  return (
-    <div className="about">
-        <div className="left">
-            <h1>Who I am ?</h1>
-            <p>My name is Kevin Munto and I am a student at Efrei Paris, an engineering school in computer science.
-                Since I was a child, I have been passionate about video games and new technologies, 
-                which allowed me to discover the vast universe of computer science.
-                In parallel to my studies, I have several passions which allow me to 
-                stimulate myself such as football, gym and video games.
-            </p>
-            <Link to="/contact/">
-                <button className='btn'>Contact</button>
-            </Link>
-            <Link to="../assets/MUNTO_KEVIN_CV.pdf" target='_blank' download >
-                <button className='btn' >Download My Cv</button>
-            </Link>
-        </div>
+    const cv = () => {
+        const cvUrl = "../assets/MuntoKevinCV.pdf";
+        const link = document.createElement("a");
+        link.href = cvUrl;
+        link.download = "MuntoKevinCV.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);  
+    };
 
-        <div className="right">
-            <div className="img-container">
-                <div className='img-stack top'>
-                    <img src={Kevin} className='img' alt='kevinm'/>
+    return (
+        <div className="about">
+            <div className="left">
+                <h1>Who I am ?</h1>
+                <p>My name is Kevin Munto and I am a student at Efrei Paris, an engineering school in computer science.
+                    Since I was a child, I have been passionate about video games and new technologies, 
+                    which allowed me to discover the vast universe of computer science.
+                    In parallel to my studies, I have several passions which allow me to 
+                    stimulate myself such as football, gym and video games.
+                </p>
+                <Link to="/contact/">
+                    <button className='btn'>Contact</button>
+                </Link>
+
+                {/*<Link to="../assets/MuntoKevinCV.pdf" target='_blank' download >
+                    <button className='btn' >Download My Cv</button>
+                </Link>*/ }
+                
+                <a href="../assets/MuntoKevinCV.pdf" download="MuntoKevinCV.pdf">
+                    <button className='btn' >Download My Cv</button>
+                </a>
+                
+            </div>
+
+            <div className="right">
+                <div className="img-container">
+                    <div className='img-stack top'>
+                        <img src={Kevin} className='img' alt='kevinm'/>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default AboutContent
+
+// <button className='btn' onClick={cv} href="../assets/MuntoKevinCV.pdf">Download My Cv</button>
